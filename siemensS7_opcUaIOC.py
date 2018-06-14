737f2b174763cb88f4a74d814cd1588b8dadf52c
Allan Serra Braga Bugyi <allan.bugyi@lnls.br>
2018-06-14 16:56:09 -0300

    Creation of methods to monitor, read and write on the specified PVs. Writing to OPC UA Nodes functionality still needs to be tested. HMI Nodes need to be mapped to PVs.

--- siemensS7_opcUaIOC.py
+++ siemensS7_opcUaIOC.py
@@ -1,4 +1,6 @@
 # -*- coding: utf8 -*-
+import time
+
 from pkg_resources import require
 require('cothread==2.14')
 require('epicsdbbuilder==development')
@@ -12,9 +14,8 @@ from opcua import Client, ua
 #Set IOC's name
 builder.SetDeviceName('CP_HVAC')
 
-#Create PVs
-#PLC INPUT PVs
-pv_23B18            = builder.longIn("23B18")		#Temperatura precisão Aquecedor Fino
+#Create INPUT PVs
+pv_23B18            = builder.longIn("23B18", )		#Temperatura precisão Aquecedor Fino
 pv_23B19            = builder.longIn("23B19")		#Temperatura Ambiente 1
 pv_23B20            = builder.longIn("23B20")		#Temperatura Ambiente 2
 pv_23B21            = builder.longIn("23B21")		#Temperatura Ambiente 3
@@ -55,7 +56,7 @@ pv_StatusContatorAQ1	= builder.boolIn("StatusContatorAQ1", ZNAM=False, ONAM=True
 pv_StatusContatorAQ1_1A = builder.boolIn("StatusContatorAQ1-1A", ZNAM=False, ONAM=True) #Estado Contator AQ1.1A
 pv_StatusDisjuntorMotor1 = builder.boolIn("StatusDisjuntorMotor1", ZNAM=False, ONAM=True) #Estado Disjuntor Motor 1
 
-#PLC OUTPUT PVs
+#Create OUTPUT PVs
 pv_AQ_Reserve		= builder.longOut("AQ_Reserve")		#Inverosr (2)
 pv_CP_PLUS_CP_23W3	= builder.longOut("CP_+_CP-23W3")	#Aquecedor Grosso
 pv_CP_23M10		= builder.longOut("CP-23M10")		#Damper 3 Insulflamento - FCV152
@@ -75,7 +76,7 @@ pv_ResetSecuritySystem	= builder.boolOut("Reset_Security_System")#Reset Security
 
 #Create OPCUA client
 client = Client("opc.tcp://10.2.121.202:4840")
-
+ 
 try:
 	#Establish connection with OPC UA Server
 	client.connect()
@@ -85,89 +86,55 @@ try:
      
 	#OPC UA parent Node from OUTPUT PVs
 	parentNode_OUTPUT_PVs = client.get_node("ns=3;s=Outputs")
+	
+	#function for monitoring the specified INPUT PV, equivalent to camonitor Channel Access command
+	def monitorPV (scan_period, pv):	
+		#preparing string equivalent to BrowseName field format from the Node (which represents the PV) in OPC UA Server address space
+		pv_name_str = pv.name 				#storing the pv's complete name 
+		record_name = pv_name_str[8:]			#removing the Device prefix from the pv's name
+		opcua_node_browseName = "3:"+record_name 	#adding the level (in this case 3) and setting the BrowseName 
+		#executes till user terminates		
+		try:		
+			while True:
+				opcua_childNode_pv = parentNode_INPUT_PVs.get_child(opcua_node_browseName)
+				(pv).set(int (opcua_childNode_pv.get_value()))
+				print((pv.name) + "\t" + str((pv).get()))
+				time.sleep(scan_period)	
+		except KeyboardInterrupt, e:
+			print "\n- Stopped Monitoring -"
+	
+	#functions to read and write to specific OUTPUT PV, equivalent to caget and caput Channel Access commands
+	def readPV (pv):
+		#preparing string equivalent to BrowseName field format from the Node (which represents the PV) in OPC UA Server address space
+		pv_name_str = pv.name 				#storing the pv's complete name 
+		record_name = pv_name_str[8:]			#removing the Device prefix from the pv's name
+		opcua_node_browseName = "3:"+record_name 	#adding the level (in this case 3) and setting the BrowseName 
+		opcua_childNode_pv = parentNode_OUTPUT_PVs.get_child(opcua_node_browseName)
+		(pv).set(int (opcua_childNode_pv.get_value()))
+		print((pv.name) + "\t" + str((pv).get()))
 
-	#Read routine for the PVs
-	pv_23B18_aux            = parentNode_INPUT_PVs.get_child("3:23B18")
-	pv_23B18.set(int (pv_23B18_aux.get_value()))
-	pv_23B19_aux            = parentNode_INPUT_PVs.get_child("3:23B19")
-	pv_23B19.set(pv_23B19_aux.get_value())
-	pv_23B20_aux            = parentNode_INPUT_PVs.get_child("3:23B20")
-	pv_23B20.set(pv_23B20_aux.get_value())
-	pv_23B21_aux            = parentNode_INPUT_PVs.get_child("3:23B21")
-	pv_23B21.set(pv_23B21_aux.get_value())
-	pv_23B22_aux            = parentNode_INPUT_PVs.get_child("3:23B22")
-	pv_23B22.set(pv_23B22_aux.get_value())
-	pv_23B23_aux            = parentNode_INPUT_PVs.get_child("3:23B23")
-	pv_23B23.set(pv_23B23_aux.get_value())
-	pv_23B24_H_aux          = parentNode_INPUT_PVs.get_child("3:23B24_H")
-	pv_23B24_H.set(pv_23B24_H_aux.get_value())
-	pv_23B24_T_aux          = parentNode_INPUT_PVs.get_child("3:23B24_T")
-	pv_23B24_T.set(pv_23B24_T_aux.get_value())
-	pv_CP_23B10_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B10")
-	pv_CP_23B10.set(pv_CP_23B10_aux.get_value())
-	pv_CP_23B11_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B11")
-	pv_CP_23B11.set(pv_CP_23B11_aux.get_value())
-	pv_CP_23B13_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B13")
-	pv_CP_23B13.set(pv_CP_23B13_aux.get_value())
-	pv_CP_23B14_H_aux       = parentNode_INPUT_PVs.get_child("3:CP-23B14_H")
-	pv_CP_23B14_H.set(pv_CP_23B14_H_aux.get_value())
-	pv_CP_23B14_T_aux       = parentNode_INPUT_PVs.get_child("3:CP-23B14_T")
-	pv_CP_23B14_T.set(pv_CP_23B14_T_aux.get_value())
-	pv_CP_23B15_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B15")
-	pv_CP_23B15.set(pv_CP_23B15_aux.get_value())
-	pv_CP_23B16_H_aux       = parentNode_INPUT_PVs.get_child("3:CP-23B16_H")
-	pv_CP_23B16_H.set(pv_CP_23B16_H_aux.get_value())
-	pv_CP_23B16_T_aux       = parentNode_INPUT_PVs.get_child("3:CP-23B16_T")
-	pv_CP_23B16_T.set(pv_CP_23B16_T_aux.get_value())
-	pv_CP_23B17_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B17")
-	pv_CP_23B17.set(pv_CP_23B17_aux.get_value())
-	pv_CP_23B25_H_aux       = parentNode_INPUT_PVs.get_child("3:CP-23B25_H")
-	pv_CP_23B25_H.set(pv_CP_23B25_H_aux.get_value())
-	pv_CP_23B25_T_aux       = parentNode_INPUT_PVs.get_child("3:CP-23B25_T")
-	pv_CP_23B25_T.set(pv_CP_23B25_T_aux.get_value())
-	pv_CP_23B26_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B26")
-	pv_CP_23B26.set(pv_CP_23B26_aux.get_value())
-	pv_CP_23B27_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B27")
-	pv_CP_23B27.set(pv_CP_23B27_aux.get_value())
-	pv_CP_23B28_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B28")
-	pv_CP_23B28.set(pv_CP_23B28_aux.get_value())
-	pv_CP_23B29_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B29")
-	pv_CP_23B29.set(pv_CP_23B29_aux.get_value())
-	pv_CP_23B30_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B30")
-	pv_CP_23B30.set(pv_CP_23B30_aux.get_value())
-	pv_CP_23B31_aux         = parentNode_INPUT_PVs.get_child("3:CP-23B31")
-	pv_CP_23B31.set(pv_CP_23B31_aux.get_value)
-	pv_CP_23M10_RET_aux     = parentNode_INPUT_PVs.get_child("3:CP-23M10_RET")
-	pv_CP_23M10_RET.set(pv_CP_23M10_RET_aux.get_value())
-	pv_CP_23M7_RET_aux      = parentNode_INPUT_PVs.get_child("3:CP-23M7_RET")
-	pv_CP_23M7_RET.set(pv_CP_23M7_RET_aux.get_value())
-	pv_CP_23M8_RET_aux      = parentNode_INPUT_PVs.get_child("3:CP-23M8_RET")
-	pv_CP_23M8_RET.set(pv_CP_23M8_RET_aux.get_value())
-	pv_CP_23M9_FDB_Hz_aux   = parentNode_INPUT_PVs.get_child("3:CP-23M9_FDB_Hz")
-	pv_CP_23M9_FDB_Hz.set(pv_CP_23M9_FDB_Hz_aux.get_value())
-	pv_CP_23M9_Status_aux   = parentNode_INPUT_PVs.get_child("3:CP-23M9_Status")
-	pv_CP_23M9_Status.set(pv_CP_23M9_Status_aux.get_value())
-	pv_CP_23X1_RET_aux      = parentNode_INPUT_PVs.get_child("3:CP-23X1_RET")
-	pv_CP_23X1_RET.set(pv_CP_23X1_RET_aux.get_value())
-	pv_Emergencia_Gases_aux = parentNode_INPUT_PVs.get_child("3:Emergencia_Gases")
-	pv_Emergencia_Gases.set(pv_Emergencia_Gases_aux.get_value())
-	pv_StatusContatorAQ1_aux= parentNode_INPUT_PVs.get_child("3:Status Contator AQ1") 
-	pv_StatusContatorAQ1.set(pv_StatusContatorAQ1_aux.get_value())
-	pv_StatusContatorAQ1_1A_aux = parentNode_INPUT_PVs.get_child("3:Status Contator AQ1.1A")
-	pv_StatusContatorAQ1_1A.set(pv_StatusContatorAQ1_1A_aux.get_value())
-	pv_StatusDisjuntorMotor1_aux = parentNode_INPUT_PVs.get_child("3:Status Disjuntor Motor 1") 
-	pv_StatusDisjuntorMotor1.set(pv_StatusDisjuntorMotor1_aux.get_value())
+	def writePV (pv, value):
+		#preparing string equivalent to BrowseName field format from the Node (which represents the PV) in OPC UA Server address space
+		pv_name_str = pv.name 				#storing the pv's complete name 
+		record_name = pv_name_str[8:]			#removing the Device prefix from the pv's name
+		opcua_node_browseName = "3:"+record_name 	#adding the level (in this case 3) and setting the BrowseName 
+		opcua_childNode_pv = parentNode_OUTPUT_PVs.get_child(opcua_node_browseName)
+		(pv).set(int (opcua_childNode_pv.get_value()))
+		print((pv.name) + "\tOld value: " + str((pv).get()))
+		opcua_childNode_pv.set_value(value)
+		(pv).set(int (opcua_childNode_pv.get_value()))
+		print((pv.name) + "\tNew value: " + str((pv).get()))	
 
-	#pv_Emergencia_Gases = parentNode_PVs.get_child("3:Emergencia_Gases")
-     	#pv_Temperatura_CP_1.set(bool (pv.get_value()))
 
+	# Run the IOC.  This is boilerplate, and must always be done in this order,
+	# and must always be done after creating all PVs.
+	builder.LoadDatabase()
+	softioc.iocInit()
+	
+	softioc.interactive_ioc(globals())
 finally:
-    client.disconnect()
+	client.disconnect()
 
-# Run the IOC.  This is boilerplate, and must always be done in this order,
-# and must always be done after creating all PVs.
-builder.LoadDatabase()
-softioc.iocInit()
-pv_23B18.set(10)
-softioc.interactive_ioc(globals())
+
+
 
